32f26f903aebe9171f986d73203379a08320fd3d
lamisa <zlq765@usak.ca>
2026-01-21 13:39:49 -0600

    worked and changed the server.js file with week1 version as per lecture slides.

--- Personal_Practice/week1/server.js
+++ Personal_Practice/week1/server.js
@@ -1,20 +1,63 @@
+/**
+ * Version with named handler functions
+ * This version breaks the logic into separate functions
+ * this one is clean separation of concerns
+ * Each route has its own function
+ * Easier to test and maintain
+ * Same behavior as before
+*/
+
+
+
+
+// This tells JavaScript:
+// be stricter 
+// catch more errors
+// prevent accidental global variables
+// enforce cleaner syntax
 'use strict';
 
-const express = require('express');
+var express = require('express');
+// App
+var app = express();
 
 // Constants
-const PORT = process.env.PORT || 3002;
-const HOST = '0.0.0.0';
+// const PORT = process.env.PORT || 3002;
+// const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/welcome', (req, res) => {
-  res.send('Welcome to CMPT 353 Tutorials');
-});
 
-app.get('/', (req, resp) => { console.log(req.originalUrl); resp.send('hello world'); });
-app.get('/hello', (req, resp) => { console.log(req.originalUrl); resp.send('hello'); });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+// Handler for '/' HOME
+function handleRootRequest(req, res)
+{
+  console.log(req.originalUrl);
+  res.send('hello world!');
+}
+
+
+// Handler for '/hello' 
+function handleHelloRequest(req, res)
+{
+  console.log(req.originalUrl);
+  res.send('hello!');
+}
+
+// Handler for file server
+function serveStaticFiles(req, res, next)
+{
+  express.static('pages')(req, res, next);
+}
+
+
+// routing 
+app.get('/', handleRootRequest);
+app.get('/hello', handleHelloRequest);
+app.get('/web', serveStaticFiles);
+
+
+// start server
+function startServer()
+{
+  console.log('Server listening on port 3002');
+}
+app.listen(3002, startServer);
