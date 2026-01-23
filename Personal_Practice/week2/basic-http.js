acc7cd4b90354b8b771832fb52cef1c01fbfa6f1
lamisa <zlq765@usak.ca>
2026-01-22 22:37:18 -0600

    Changed the port number and also fixed the file name inside the server.js file.

--- Personal_Practice/week2/basic-http.js
+++ Personal_Practice/week2/basic-http.js
@@ -9,7 +9,7 @@ const http = require('http');
 const server = http.createServer((req, res) =>
     {
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello, world!!\n');
+        res.end('Hello, world from basic-http server !!\n');
     });
 
 
--- Personal_Practice/week2/server.js
+++ Personal_Practice/week2/server.js
@@ -1,37 +1,28 @@
+
 /**
- * Mini exercise
- * Add a new route: GET /hello that returns “Hello!”
- * Add GET /count that returns the current count as JSON 
- * Bonus: add /reset to set count back to 0
-*/
+ * server.js
+ * Connect HTML → Node server
+ * */
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const express = require("express");
-const app = express();
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "public", "basics1.html");
 
-let count = 0;
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Error loading HTML file");
+      return;
+    }
 
-// Home route
-app.get("/", (req, res) => {
-  count++;
-  res.send("This server has handled " + count + " requests");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 });
 
-// /hello route
-app.get("/hello", (req, res) => {
-  res.send("Hello!");
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
 
-// /count route (returns JSON)
-app.get("/count", (req, res) => {
-  res.json({ count });
-});
-
-// /reset route
-app.get("/reset", (req, res) => {
-  count = 0;
-  res.send("Count reset to 0");
-});
-
-app.listen(3002, () => {
-  console.log("Server running on http://localhost:3002");
-});
